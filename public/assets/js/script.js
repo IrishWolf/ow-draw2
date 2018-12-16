db1ada8e6358d6f41f295493303c86bafef75ff6
IrishWolf <36819852+IrishWolf@users.noreply.github.com>
2018-12-16 12:55:46 +0100

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,7 +4,7 @@
 // TODO: Allow server to handle the random number generator so the revealed letters are equal among all devices
     var IO = {
         init : function(){
-            var url = 'https://ow-draw.herokuapp.com/';
+            var url = 'http://ow-draw.herokuapp.com/';
             IO.socket = io.connect(url);
             IO.bindEvents();
 
